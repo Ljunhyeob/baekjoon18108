a6d63bebea19e698fa5e85df9a9669c9ed5ef0ae
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-03 22:39:12 +0900

    Add files via upload

---
+++ baekjoon18108/baekjoon18108/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  baekjoon18108
+//
+//  Created by 이준협 on 2023/01/03.
+//
+import Foundation
+
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+var a = Int(lineArr[0])!
+print(a-543)
